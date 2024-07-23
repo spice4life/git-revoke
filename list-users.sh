6b037f35f0bf6c8b3d22417bf864ebf83d08bedf
msumana <ubuntu@ip-172-31-87-195.ec2.internal>
2024-07-23 04:23:19 +0000

    Added list-users

---
+++ list-users.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+##########
+# About: Script is to retrieve users who have access to git repository
+#
+# Input:export username and token
+#
+# Owner:Sumana
+#
+#
+###########################
+helper
+
+# GitHub API URL
+API_URL="https://api.github.com"
+
+# GitHub username and personal access token
+USERNAME=$username
+TOKEN=$token
+
+# User and Repository information
+REPO_OWNER=$1
+REPO_NAME=$2
+
+# Function to make a GET request to the GitHub API
+function github_api_get {
+    local endpoint="$1"
+    local url="${API_URL}/${endpoint}"
+
+    # Send a GET request to the GitHub API with authentication
+    curl -s -u "${USERNAME}:${TOKEN}" "$url"
+}
+
+# Function to list users with read access to the repository
+function list_users_with_read_access {
+    local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
+
+    # Fetch the list of collaborators on the repository
+    collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login')"
+
+    # Display the list of collaborators with read access
+    if [[ -z "$collaborators" ]]; then
+        echo "No users with read access found for ${REPO_OWNER}/${REPO_NAME}."
+    else
+        echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}:"
+        echo "$collaborators"
+    fi
+}
+
+# Helper script
+
+function helper {
+	expected_cmd_args=2
+	if [ $# -ne $expected_cmd_args]; then
+		echo "Please execute the script with reequired cmd args"
+		echo "asd"
+	else
+		echo "execute script"
+	fi		
+}
+
+
+# Main script
+
+echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
+list_users_with_read_access
